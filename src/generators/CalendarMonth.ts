9431bddeb2342dcb37aa200a5c4991e8c786e889
Petr Zenin <mr@adminoid.com>
2021-09-14 03:20:00 +0300

    getting calendar map and test it

--- src/generators/CalendarMonth.ts
+++ src/generators/CalendarMonth.ts
@@ -11,10 +11,49 @@ export interface ICalendarMonth {
 export class CalendarMonth implements ICalendarMonth {
   selectedMonth: Date
   boundaries: TBoundaries
+  lastDayOfMonth: Date
+  calendar: Date[][]
 
   constructor (year: number, month: number) {
     this.selectedMonth = new Date(year, month)
+    this.lastDayOfMonth = new Date(
+      this.selectedMonth.getFullYear(),
+      this.selectedMonth.getMonth() + 1,
+      0
+    )
     this.boundaries = this.getCalendarBoundaries()
+    this.calendar = this.getCalendarMap()
+  }
+
+  getCalendarMap (): Date[][] {
+    const weeks = []
+    let days = []
+    let weekDay = 1
+    for (
+      const dayIterator = new Date(this.boundaries.first);
+      dayIterator <= this.boundaries.last;
+      dayIterator.setDate(dayIterator.getDate() + 1)
+    ) {
+      days.push(new Date(dayIterator))
+      if (weekDay % 7 === 0) {
+        weekDay = 1
+        weeks.push(days)
+        days = []
+      } else {
+        weekDay++
+      }
+    }
+    return weeks
+  }
+
+  getCalendarBoundaries (): TBoundaries {
+    const current = new Date(this.selectedMonth)
+    const firstDayInCalendar = this.getFirstWeekDay(current)
+    const lastDayInCalendar = this.getLastWeekDay(this.lastDayOfMonth)
+    return {
+      first: firstDayInCalendar,
+      last: lastDayInCalendar
+    }
   }
 
   /**
@@ -35,20 +74,5 @@ export class CalendarMonth implements ICalendarMonth {
     const lastDay = new Date(date)
     if (day !== 1) lastDay.setHours(24 * (7 - day))
     return lastDay
-  }
-
-  getCalendarBoundaries (): TBoundaries {
-    const current = new Date(this.selectedMonth)
-    const firstDayInCalendar = this.getFirstWeekDay(current)
-    const lastDayInMonth = new Date(
-      this.selectedMonth.getFullYear(),
-      this.selectedMonth.getMonth() + 1,
-      0
-    )
-    const lastDayInCalendar = this.getLastWeekDay(lastDayInMonth)
-    return {
-      first: firstDayInCalendar,
-      last: lastDayInCalendar
-    }
   }
 }
--- tests/unit/generators/CalendarMonth.spec.ts
+++ tests/unit/generators/CalendarMonth.spec.ts
@@ -1,11 +1,17 @@
 import { CalendarMonth } from '@/generators/CalendarMonth'
 
 describe('generators/CalendarMonth.ts', () => {
+  const CMInstance = new CalendarMonth(2021, 8)
+
   it('should get selected calendar month', () => {
-    const CMInstance = new CalendarMonth(2021, 10)
     expect(CMInstance.boundaries).toMatchObject({
       first: new Date('2021-10-31T21:00:00.000Z'),
       last: new Date('2021-12-04T21:00:00.000Z')
     })
+  })
+
+  it('should get calendar map', () => {
+    expect(CMInstance.calendar.length).toEqual(5)
+    expect(CMInstance.calendar[0].length).toEqual(7)
   })
 })
